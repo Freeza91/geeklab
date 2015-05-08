c22b02f94eccc5fb4a94b57373c9d588391aeed7
wangxiaod <wx920804@gmail.com>
2015-05-08 16:01:25 +0800

    测试用户信息提交

---
+++ app/assets/javascripts/testers/info.js
@@ -0,0 +1,44 @@
+$(function () {
+  if(!$('body').hasClass('testers_new')) {
+    return false;
+  }
+  $('.info-form .submit').on('click', function (event) {
+    event.preventDefault();
+
+    var data = {};
+    var $form = $('.info-form');
+    var $infoSet = $form.find('.form-group');
+    $infoSet.each(function(index,item) {
+      var $item = $(item)
+      var key = $item.find('.info-title').data('name');
+      var type = $item.data('type');
+      var value = '';
+      switch(type){
+        case 'text':
+          value = $item.find('input').val();
+        break;
+        case 'radio':
+          value = $item.find('input:radio:checked').val();
+        break;
+        case 'checkbox':
+          value = [];
+          $checkboxSet = $item.find('input:checkbox:checked');
+          $checkboxSet.each(function (index, checkbox) {
+            value.push($(checkbox).val());
+          });
+        break;
+        case 'select':
+          $selectSet = $item.find('select');
+          $selectSet.each(function (index, select) {
+            if(index > 0){
+              value += '-';
+            } 
+            value += $(select).val();
+          });
+        break;
+      }
+      data[key] = value;
+    });
+    console.log(data);
+  });
+});

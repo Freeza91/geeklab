7968eee726683a0774d62d29ba781d88e6c29630
rudy <347212291@qq.com>
2015-05-13 14:53:01 +0800

    add User to tester/pm

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
   include Rememberable
   include Recoverable
   include Mobileable
+
+  def to_tester
+    Tester.find_by(id: id)
+  end
+
+  def to_pm
+    Pm.find_by(id: id)
+  end
 end

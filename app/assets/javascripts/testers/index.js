aa3370cb99e168e321394e1e5ec55a076640d7c9
wangxiaod <wx920804@gmail.com>
2015-05-14 15:25:40 +0800

    测试用户页按钮

--- app/assets/javascripts/testers/index.js
+++ app/assets/javascripts/testers/index.js
@@ -3,11 +3,16 @@ $(function () {
     return false;
   }
   $('.temp').on('click', function() {
+    var $this = $(this);
+    var approved = $this.data('approved');
     var $modal = $('#info-modal');
-    $modal.find('.title').text('怎么成为体验师');
-    $modal.find('.content').text('完成新手任务即可成为体验师');
+    if(approved) {
+      $modal.find('.title').text('怎么体验产品');
+      $modal.find('.content').text('任务会以邮件的方式发送到你的邮箱里，请注意查收');
+    } else {
+      $modal.find('.title').text('怎么成为体验师');
+      $modal.find('.content').text('完成新手任务即可成为体验师，新手任务已经在你的邮箱里了，赶快去完成吧!');
+    }
     $modal.modal();
-    //怎么体验产品
-    //任务会以邮件的方式发送到你的邮箱里，请注意查收
   });
 });

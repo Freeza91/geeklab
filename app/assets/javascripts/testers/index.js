c0398a510a41742d103c95eea64c8db19896d301
wangxiaod <wx920804@gmail.com>
2015-05-15 14:41:03 +0800

    添加发送新手任务邮件的接口

--- app/assets/javascripts/testers/index.js
+++ app/assets/javascripts/testers/index.js
@@ -9,10 +9,21 @@ $(function () {
     if(approved) {
       $modal.find('.title').text('怎么体验产品');
       $modal.find('.content').text('任务会以邮件的方式发送到你的邮箱里，请注意查收');
+      $modal.modal();
     } else {
-      $modal.find('.title').text('怎么成为体验师');
-      $modal.find('.content').text('完成新手任务即可成为体验师，新手任务已经在你的邮箱里了，赶快去完成吧!');
+      $.ajax({
+        url: '/users/mailers/send_novice_task'
+      })
+      .done(function (data, status) {
+        if(data.status === 0 && data.code === 1) {
+          $modal.find('.title').text('怎么成为体验师');
+          $modal.find('.content').text('完成新手任务即可成为体验师，新手任务已经在你的邮箱里了，赶快去完成吧!');
+          $modal.modal();
+        }
+      })
+      .error(function (errors, status) {
+        console.log(errors);
+      });
     }
-    $modal.modal();
   });
 });

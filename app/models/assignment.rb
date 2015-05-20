fba539efba845c5fb75cb474b69a39529aec2881
rudy <347212291@qq.com>
2015-05-20 16:25:15 +0800

    update assignment scope

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,11 +1,38 @@
 class Assignment < ActiveRecord::Base
 
-  scope :all_task,      -> { where.not(status: "delete") }
-  scope :wait_upload,   -> { where(status: "wait_upload") }
-  scope :wait_check,    -> { where(status: "wait_check") }
-  scope :checking,      -> { where(status: "checking") }
-  scope :success,       -> { where(status: "success") }
-  scope :failed,        -> { where(status: "failed") }
+  scope :my_task, -> { joins(:tester).where("users.email = ? and status = ?", 'rudy@geekpark.net', 'hello') }     # testers is not table, this just a example
+
+  # scope :expired,      -> { joins(:project).where("projects.expired_at > ?", Time.now - 10.minutes) }
+  # scope :not_expired,  -> { joins(:project).where("projects.expired_at > ?", Time.now - 10.minutes) }
+
+  scope :not_take_part,-> { where(status: "new") }
+  scope :ing,  -> { where('status in (?)', ['wait_check', 'checking', 'not_accept']) }
+  scope :done, -> { where(status: "success") }
 
   belongs_to :tester
+
+  class << self
+
+    def take_part_ing
+      # not_expired.ing
+    end
+
+    def take_part_done
+      # not_expired.done + take_part_ing.expired
+    end
+
+    def missing
+      # expired.not_take_part
+    end
+
+    def new_tasks
+      # not_expired.not_take_part
+    end
+
+    def test
+      assignments = User.find(1).to_tester.assignments
+      assignments.ing + assignments.done + assignments.not_take_part
+    end
+
+  end
 end

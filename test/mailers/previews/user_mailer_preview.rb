4054e508f80a3125d5595b76dc6bc1299e64c482
rudy <347212291@qq.com>
2015-06-03 18:15:04 +0800

    add rails.env email preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,21 +1,23 @@
-class UserMailerPreview < ActionMailer::Preview
-  def welcome
-    UserMailer.welcome('rudy@geekpark.net', '123456')
-  end
+if Rails.env.development?
+  class UserMailerPreview < ActionMailer::Preview
+    def welcome
+      UserMailer.welcome('rudy@geekpark.net', '123456')
+    end
 
-  def reset_password
-    UserMailer.reset_password( User.first.id, "http://geeklab.cc")
-  end
+    def reset_password
+      UserMailer.reset_password( User.first.id, "http://geeklab.cc")
+    end
 
-  def novice_task
-    UserMailer.novice_task('rudy@geekpark.net')
-  end
+    def novice_task
+      UserMailer.novice_task('rudy@geekpark.net')
+    end
 
-  def novice_task_approved
-    UserMailer.novice_task_approved('rudy@geekpark.net')
-  end
+    def novice_task_approved
+      UserMailer.novice_task_approved('rudy@geekpark.net')
+    end
 
-  def new_task_notice
-    UserMailer.new_task_notice('rudy@geekpark.net', '新手任务通知')
+    def new_task_notice
+      UserMailer.new_task_notice('rudy@geekpark.net', '新手任务通知')
+    end
   end
 end

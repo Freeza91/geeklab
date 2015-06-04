145c186985d6b607d3b5034724a27e7b08b688de
rudy <347212291@qq.com>
2015-06-04 15:53:03 +0800

    update email

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -59,7 +59,7 @@ class Assignment < ActiveRecord::Base
       name = self.project.name
       email_to = self.tester.tester_infors.first.email_contract || email
       UserMailer.video_check_failed(email_to, name, task_url).deliver_later if status == "not_accept"
-      UserMailer.video_check_failed(email_to, name, task_url).deliver_later if status == "success"
+      UserMailer.video_check_success(email_to, name, task_url).deliver_later if status == "success"
     end
   end
 end

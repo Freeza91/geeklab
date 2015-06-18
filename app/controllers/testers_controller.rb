2d6599d8a06744c7e7f50cf392c067231dc52e64
wangxiao <wx920804@gmail.com>
2015-06-18 15:52:25 +0800

    bug fix

--- app/controllers/testers_controller.rb
+++ app/controllers/testers_controller.rb
@@ -36,7 +36,7 @@ class TestersController < ApplicationController
   end
 
   def edit
-    @devices = ['iPhone', 'iPad', 'Andoriod Phone', 'Andoriod Pad']
+    @devices = ['iPhone', 'iPad', 'Android Phone', 'Android Pad']
     @personality = ['温柔', '粗犷', '活泼', '老城', '内向', '开朗', '豪爽', '沉默', '急躁', '稳重']
     @interests = ['足球', '健身', '旅游', '二次元', '音乐', '看书', '电影', '星座']
     @tester_infor = @current_user.to_tester.tester_infors[0]

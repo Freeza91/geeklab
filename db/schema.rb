f75ffb1cdff981c48ffe328c7ebeef585b569215
rudy <347212291@qq.com>
2015-07-06 15:42:38 +0800

    add addition to sku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706071358) do
+ActiveRecord::Schema.define(version: 20150706074122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20150706071358) do
     t.integer  "good_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "addition"
   end
 
   create_table "tasks", force: :cascade do |t|

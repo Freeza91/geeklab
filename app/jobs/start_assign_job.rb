6747a96d8ffc5be27150b5095c9acc31d59ed1a7
rudy <347212291@qq.com>
2015-07-09 16:44:51 +0800

    fix bug

--- app/jobs/start_assign_job.rb
+++ app/jobs/start_assign_job.rb
@@ -4,6 +4,6 @@ class StartAssignJob < ActiveJob::Base
   def perform(project_id)
     AssignmentCategory.assignment_to_tester(project_id)
     project = Project.find_by(id: project_id)
-    project.update_column(status: 'underway') if project && project.try(:status) == 'success'
+    project.update_column(:status, 'underway') if project && project.try(:status) == 'success'
   end
 end

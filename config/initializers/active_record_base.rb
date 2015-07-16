cc0fb975fda3292950ece98d3ecbd3652ce749eb
rudy <347212291@qq.com>
2015-07-16 11:55:33 +0800

    add hash id params

--- config/initializers/active_record_base.rb
+++ config/initializers/active_record_base.rb
@@ -1,26 +1,26 @@
-$hashids = Hashids.new Settings.hashids_secret_token, 2
+$hashids = Hashids.new Settings.hashids_secret_token, 8
 
 class ActiveRecord::Base
 
-  # # 加密id做参数
-  # def to_param
-  #   encode_id
-  # end
+  # 加密id做参数
+  def to_params
+    encode_id
+  end
 
-  # # 加密id
-  # def encode_id
-  #   $hashids.encode(id)
-  # end
+  # 加密id
+  def encode_id
+    $hashids.encode(id)
+  end
 
-  # # 解密id
-  # def decode_id
-  #   $hashids.decode(id)[0]
-  # end
+  # 解密id
+  def decode_id(hash_id)
+    $hashids.decode(hash_id)[0]
+  end
 
-  # def self.find_by(opt = {})
-  #   hash_id = opt[:id]
-  #   opt[:id] = $hashids.decode(hash_id)[0] if hash_id
-  #   where(opt).take
-  # end
+  def self.find_by(opt = {})
+    hash_id = opt[:id]
+    opt[:id] = $hashids.decode(hash_id)[0] if hash_id
+    where(opt).take
+  end
 
 end

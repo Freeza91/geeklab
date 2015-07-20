3de5c8cd0bdcd1b13f9ff92dcc05f8a91c36a5f5
rudy <347212291@qq.com>
2015-07-20 11:43:14 +0800

    update goods stores index

--- app/controllers/stores/base_controller.rb
+++ app/controllers/stores/base_controller.rb
@@ -3,8 +3,15 @@ class Stores::BaseController < ApplicationController
   layout 'stores/layouts/stores_application'
 
   def index
-    @goods = Good.display.includes(:pictures).page(params[:page]).per(6)
-    render 'stores/index'
+    json = { status: 0, code: 1, goods:[] }
+
+    @goods = Good.display.includes(:pictures).page(params[:page]).per(9)
+    @goods.each do |good|
+      json[:goods] << good.to_json_with_pictures
+    end
+
+    render json: json
+
   end
 
   def authenticate
--- app/models/good.rb
+++ app/models/good.rb
@@ -28,4 +28,13 @@ class Good < ActiveRecord::Base
     end
   end
 
+  def to_json_with_pictures
+    {
+      id: self.to_params,
+      name: name,
+      cost: cost,
+      pictures: self.pictures.collect {|picture| picture.url.try(:url) }
+    }
+  end
+
 end

c2094acf67f8444cfd5932d189c6a7eb9711b8ea
rudy <347212291@qq.com>
2015-07-28 19:22:26 +0800

    fix routes match all

--- config/routes.rb
+++ config/routes.rb
@@ -102,5 +102,6 @@ Rails.application.routes.draw do
   match '/404', to: 'errors#file_not_found', via: :all
   match '/422', to: 'errors#unprocessable', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
+  match "*path", to: "errors#file_not_found", via: :all
 
 end

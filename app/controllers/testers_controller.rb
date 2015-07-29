49a6ec8869aca0c6c30e53b67424bd777486be36
rudy <347212291@qq.com>
2015-07-29 09:18:04 +0800

    update tester infor

--- app/controllers/testers_controller.rb
+++ app/controllers/testers_controller.rb
@@ -47,18 +47,7 @@ class TestersController < ApplicationController
   def update
     json = { status: 0, code: 1, msg: '创建成功', url: testers_path }
 
-    if user = current_user
-      hashids = $hashids.encode(user.id)
-      unless hashids == params[:id]
-        json[:code], json[:msg] = 3, '没有权限'
-        return render json: json
-      end
-    else
-      json[:code], json[:msg] = 2, "没有登录"
-      return render json: json
-    end
-
-    tester = Tester.find_by(id: params[:id])
+    tester = current_user.to_tester.tester_infors.find_by(id: params[:id])
     if tester && tester_infors = tester.tester_infors
       @tester_infor = tester_infors.last.tap &model_block
       unless @tester_infor.save

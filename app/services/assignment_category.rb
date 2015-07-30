4424e742784ab26bfe108ee91eeec7b010bb2a22
rudy <347212291@qq.com>
2015-07-30 16:08:26 +0800

    update new_task_notice url

--- app/services/assignment_category.rb
+++ app/services/assignment_category.rb
@@ -12,7 +12,7 @@ module AssignmentCategory
           # send assignment
           Assignment.create(tester_id: infor.tester_id, project_id: project.id, status: 'new')
           # deliver email to tester
-          task_url = "#{Settings.domain}/testers/#{infor.tester_id}/assignments"
+          task_url = "#{Settings.domain}/assignments"
           mail_to = infor.tester.tester_infor.email_contract || infor.tester.email
           UserMailer.new_task_notice(mail_to, project.name, task_url).deliver_later
         end

9e2e576848e73a520d36841da00cd71464a0b246
wangxiao <wx920804@gmail.com>
2015-08-03 11:51:24 +0800

    发送重置密码邮件验证及提示

--- app/assets/javascripts/stores/password.js
+++ app/assets/javascripts/stores/password.js
@@ -12,6 +12,7 @@ $(function () {
         canSendEmail: true,
       },
       methods: {
+        checkEmail: checkEmail,
         submit: sendEmail
       }
     });
@@ -32,11 +33,23 @@ $(function () {
       }
     })
   }
+  function checkEmail (vm) {
+    var email = vm.email;
+    if(email) {
+      if(!formValid(email, 'email')) {
+        vm.hint = '邮箱格式错误';
+        vm.error = true;
+        return false;
+      }
+    }
+    return true;
+  }
+
   function formValid (value, type) {
     var result;
     switch(type){
       case 'email':
-        var emailReg = /^[0-9a-zA-Z_-]+@([0-9a-zA-Z]+.)+[a-zA-Z]$/;
+        var emailReg = /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/;
         result = emailReg.test(value);
       break;
       case 'password':
@@ -56,30 +69,18 @@ $(function () {
 
   function sendEmail (vm, event) {
     event.preventDefault();
-    if(!vm.canSendEmail) {
-      return false;
-    }
-    if(!vm.email || vm.email === '') {
+    if(!vm.email) {
       vm.hint = '请输入邮箱';
       vm.error = true;
       return false;
     }
-    if(!formValid(vm.email, 'email')) {
-      vm.hint = '邮箱格式错误';
-      vm.error = true;
+    if(!checkEmail(vm) || !vm.canSendEmail) {
       return false;
     }
 
     vm.canSendEmail = false;
     vm.error = false;
-    var intervalId = setInterval(function () {
-      vm.countDown--;
-      if(vm.countDown === 0) {
-        clearInterval(intervalId);
-        vm.countDown = 60;
-        vm.canSendEmail = true;
-      }
-    }, 1000);
+    vm.hint = '';
 
     $.ajax({
       url: '/users/mailers/send_reset_password',
@@ -93,13 +94,22 @@ $(function () {
             // 邮箱未注册
             vm.hint = '此邮箱未注册';
             vm.error = true;
+            vm.canSendEmail = true;
           break;
           case 1:
             // 发送成功
-          if(data.msg !== '') {
-            vm.mailbox = data.msg
-          }
-          vm.success = true;
+            var intervalId = setInterval(function () {
+              vm.countDown--;
+              if(vm.countDown === 0) {
+                clearInterval(intervalId);
+                vm.countDown = 60;
+                vm.canSendEmail = true;
+              }
+            }, 1000);
+            if(data.msg !== '') {
+              vm.mailbox = data.msg
+            }
+            vm.success = true;
           break;
         }
       }
@@ -146,5 +156,30 @@ $(function () {
     .error(function (errors) {
       console.log(errors);
     })
+  }
+
+  function emailRegisted(email, callback) {
+    $.ajax({
+      url: '/users/registrations/is_emails_exist',
+      data: {email: email}
+    })
+    .done(function (data, status, xhr) {
+      if(data.status === 0) {
+        switch(data.code) {
+          case 0:
+            // 已被注册
+          break;
+          case 1:
+            // 未被注册
+            if(callback !== undefined) {
+              callback();
+            }
+          break;
+        }
+      }
+    })
+    .error(function (errors, status) {
+      console.log(errors);
+    });
   }
 });

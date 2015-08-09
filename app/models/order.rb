a3c9d4dcae14b44989d11d1f513558bb0a840823
wangxiao <wx920804@gmail.com>
2015-08-09 19:40:00 +0800

    实物订单显示快递状态和物流查询链接

--- app/models/order.rb
+++ app/models/order.rb
@@ -27,16 +27,21 @@ class Order < ActiveRecord::Base
   end
 
   def to_json
-    {
+    json = {
       id: self.to_params,
       created_at: created_at.strftime('%F'),
       order_num: order_id,
+      virtual: self.virtual?,
       cost: self.good.cost,
       good_id: self.to_params(self.good_id),
       good_name: self.good.name,
       good_pic: self.good.pictures.first.url.try(:url),
       detail: self.get_order_detail
     }
+    unless self.virtual?
+      json[:express_status] = self.address.parse_status
+    end
+    json
   end
 
   def get_order_detail
@@ -50,13 +55,11 @@ class Order < ActiveRecord::Base
       if msg
         msg = msg.split('&')
         detail[:物流公司] = msg[0]
-        detail[:运单号] = msg[1]
+        detail[:快递单号] = msg[1]
       else
         detail[:物流公司] = '暂无'
-        detail[:运单号] = '暂无'
+        detail[:快递单号] = '暂无'
       end
-      express_status = self.address.parse_status
-      detail[:订单状态] = express_status
     end
     detail
   end

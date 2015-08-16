433dfed46f5adb022a3d1415b8195d749f3d73f8
wangxiao <wx920804@gmail.com>
2015-08-16 22:04:52 +0800

    首屏文字翻转动画

--- app/assets/javascripts/testers/index.js
+++ app/assets/javascripts/testers/index.js
@@ -2,6 +2,7 @@ $(function () {
   if(!$('body').hasClass('testers_index')) {
     return false;
   }
+  var rotateInterval;
   $('#fullpage').fullpage({
     //Scrolling
     css3                              : true,
@@ -29,7 +30,7 @@ $(function () {
     resize                            : true,
     paddingTop                        : '50px',
     paddingBottom                     : '0',
-    sectionsColor                     : ['#fff', '#fbf9f3', '#fff'],
+    sectionsColor                     : ['#fff', '#fbf9f3', 'rgba(0, 0, 0, .5)'],
     //fixedElements                     : '#header, .footer',
     responsiveWidth                   : 0,
     responsiveHeight                  : 0,
@@ -43,8 +44,32 @@ $(function () {
     afterLoad      : function(anchorLink, index){
       if(index === 1) {
         $('#header').removeClass('zoom-out');
+        var item = $('#slogan .dynamic b'),
+            index = 0;
+        rotateInterval = setInterval(function () {
+          var curInnerItem = $(item[index]).find('i'),
+              nextInnerItem = $(item[(index + 1) % 2]).find('i'),
+              curLen = curInnerItem.length,
+              nextLen = nextInnerItem.length,
+              lenMax = Math.max(curLen, nextLen),
+              innerIndex = 0;
+          var innerIntervalId = setInterval(function () {
+            if(innerIndex < curLen) {
+              curInnerItem[innerIndex].className = 'out';
+            }
+            if(innerIndex < nextLen) {
+              nextInnerItem[innerIndex].className = 'in';
+            }
+            innerIndex += 1;
+            if(innerIndex >= lenMax) {
+              clearInterval(innerIntervalId);
+            }
+          }, 100);
+          index = (index + 1) % 2;
+        }, 2000);
       } else {
         $('#header').addClass('zoom-out');
+        clearInterval(rotateInterval);
       }
     },
     afterRender    : function(){},

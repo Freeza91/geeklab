5cce4eb265a0ce2e47c1dd9fecb00c81556b99d5
wangxiao <wx920804@gmail.com>
2015-08-26 18:09:42 +0800

    footer永远置底

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
   def get_qiniu_source(path)
     "http://" + Settings.qiniu_bucket_domain + "/" + path
   end
+
+  def render_footer(controller_name, action_name)
+    if controller_name == 'passwords' || controller_name == 'errors'
+      return false
+    end
+    if controller_name == 'testers' && action_name == 'index'
+      return false
+    end
+
+    true
+
+  end
 end

c65f88b5b3ac5d9e79292aabc198e3f3b5186628
rudy <347212291@qq.com>
2015-09-10 16:18:00 +0800

    add admin filed to user for backend admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730091345) do
+ActiveRecord::Schema.define(version: 20150910072841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -175,6 +175,7 @@ ActiveRecord::Schema.define(version: 20150730091345) do
     t.datetime "last_view_time",         default: '2015-05-21 11:37:35'
     t.integer  "credits",                default: 0
     t.datetime "approved_time",          default: '2015-07-28 18:03:00'
+    t.integer  "admin",                  default: 0
   end
 
   add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree

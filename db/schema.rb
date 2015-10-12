c4470acf1a9b14b85c82bdc3fe5832c762aff72d
rudy <347212291@qq.com>
2015-10-12 14:28:55 +0800

    add used flag to credit record

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010032954) do
+ActiveRecord::Schema.define(version: 20151012062558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,8 +58,9 @@ ActiveRecord::Schema.define(version: 20151010032954) do
     t.integer  "bonus_credits"
     t.integer  "rating"
     t.string   "rating_type"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.boolean  "used",          default: false
   end
 
   create_table "feedbacks", force: :cascade do |t|

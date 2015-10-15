a61c6e5c82e3fb982903eeeae7a887e45ad22bff
rudy <347212291@qq.com>
2015-10-15 11:17:28 +0800

    fix bug for ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,15 +4,8 @@ class Ability
   def initialize(user)
     user ||= User.new
     ability = user.try(:admin)
-    if ability == 1
-        can :manage, [Address, Assignment, Comment, Good,
-                      Order, Picture, Project, Sku, Task,
-                      TesterInfor, UserFeature]
-
-        can :read [Pm, Tester]
-    else ability == 2
+    if ability >= 1
         can :manage, :all
     end
-
   end
 end

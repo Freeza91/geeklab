d98204d258cfc10275a0328c7aadbd39d27c3cad
rudy <347212291@qq.com>
2015-10-19 12:35:59 +0800

    fix bug for auto_update_assignment_job

--- app/jobs/auto_update_assignment_job.rb
+++ app/jobs/auto_update_assignment_job.rb
@@ -3,7 +3,9 @@ class AutoUpdateAssignmentJob < ActiveJob::Base
 
   def perform(assignment_id)
     a = Assignment.find_by(id: $hashids.encode(assignment_id))
-    a.update_attribute(:status, 'checking') if a.try(:status) == 'new' || a.try.(:status) == 'test'
+    if a && (a.status == 'new' || a.status == 'test')
+      a.update_attribute(:status, 'checking')
+    end
   end
 
 end

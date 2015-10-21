5693d0ba6df883423f0abbe0d1bc35611cf099e2
wangxiao <wx920804@gmail.com>
2015-10-22 00:51:59 +0800

    将tester_infor create, edit合为一个页面

--- app/controllers/testers_controller.rb
+++ app/controllers/testers_controller.rb
@@ -12,10 +12,10 @@ class TestersController < ApplicationController
     @personality = ['温柔', '粗犷', '活泼', '老成', '内向', '开朗', '豪爽', '沉默', '急躁', '稳重']
     @interests = ['足球', '健身', '旅游', '二次元', '音乐', '看书', '电影', '星座']
     @tester_infor = current_user.to_tester.tester_infor
-    if @tester_infor.already_finish
-      return redirect_to edit_tester_path
-    end
-    render '/testers/new'
+    #if @tester_infor.already_finish
+      #return redirect_to edit_tester_path
+    #end
+    render '/testers/edit'
   end
 
   def create

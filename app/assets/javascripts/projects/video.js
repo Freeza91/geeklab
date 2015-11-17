2cd1e9676112ec5ec35d98f8d22fe07f1d4eeedb
wangxiao <wx920804@gmail.com>
2015-11-17 15:47:59 +0800

    视频评分接口对接

--- app/assets/javascripts/projects/video.js
+++ app/assets/javascripts/projects/video.js
@@ -5,13 +5,23 @@ $(function () {
 
   var player = $('#video')[0];
 
+  var isRating = $('#is-rating').val();
+
   //$('.comment-content').on('click', function (event) {
     //var target = event.target;
     //$(this).attr('contentEditable', true);
   //});
 
   $('.rating-star').on('click', function () {
-    var rating = 5 - $(this).data('rating');
+    if(isRating === 'true' || isRating === true) {
+      return false;
+    }
+    var rating = 5 - $(this).data('rating'),
+        projectId = $('#project-id').val(),
+        assignmentId = $('#assignment-id').val();
+    sendRatingRequest(projectId, assignmentId, rating, function (data) {
+      isRating = true;
+    });
     console.log(rating);
   });
 
@@ -22,6 +32,28 @@ $(function () {
     $target.addClass('active');
     $('.video-info-section.active').hide().removeClass('active');
     $(selector).fadeIn().addClass('active');
+  }
+
+  function sendRatingRequest (projectId, assignmentId, rating, callback) {
+    var url = '/assignments/' + assignmentId + '/rating';
+    $.ajax({
+      url: url,
+      method: 'post',
+      data: {
+        project_id: projectId,
+        rating: rating
+      },
+      success: function (data) {
+        console.log(data);
+        if(data.status === 0 && data.code === 1) {
+          callback(data);
+        }
+      },
+      error: function (xhr, textStatus, errors) {
+        console.log(data);
+      }
+    });
+
   }
   $('[role="tablist"] a').on('click', toggleItemBodyContent);
 

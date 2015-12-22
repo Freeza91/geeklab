5411946353e51208650337510bc889a86558d3d7
wangxiao <wx920804@gmail.com>
2015-12-22 10:40:06 +0800

    mobile_upload valid方法修改

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -13,7 +13,7 @@ module AssignmentsHelper
   end
 
   def mobile_upload_valid(auth, assignment)
-    if auth && assignment.project.expired_at > Time.now
+    if auth && assignment.expired_time > 0
       'valid'
     else
       'invalid'
@@ -37,20 +37,6 @@ module AssignmentsHelper
   def dashboard_select_rank
     (1..5).to_a.collect{|k| [k, k] }
   end
-
-  #def isAssignmentEmpty(assignments)
-    #if assignments.size == 0
-      #return 'empty'
-    #end
-  #end
-
-  #def showBonus(credit_record)
-    #if credit_record && credit_record.used
-      #(credit_record.bonus_credits.to_i) * (credit_record.rating.to_i)
-    #else
-      #'评分奖励'
-    #end
-  #end
 
   def is_old_assignment(create_time)
     create_time < Date.new(2015, 10, 23)

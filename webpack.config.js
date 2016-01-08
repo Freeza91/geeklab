0bead64cee94871566d753d8af5fdf696bfad827
wangxiao <wx920804@gmail.com>
2016-01-08 11:32:08 +0800

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path'),
+    webpack = require('webpack');
+
+var config = module.exports = {
+  context: __dirname,
+  entry: {
+    main: './app/frontend/javascripts/main.js',
+    //sign: './app/frontend/javascripts/users/sign.js',
+    //password: './app/frontend/javascripts/users/password.js',
+    //assignment_index: './app/frontend/javascripts/assignments/assignment_index.js',
+    //assignment_join: './app/frontend/javascripts/assignments/assignment_join.js',
+  }
+};
+
+config.output = {
+  // 编译完成的文件的文件名和存放路径
+  path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+  filename: '[name]_bundle.js',
+  publicPaht: '/assets'
+};
+
+config.resolve = {
+  extensions: ['', '.js']
+};
+
+config.module = {
+
+};
+
+config.plugins = [
+]
